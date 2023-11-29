b52b76d86feae9bb810917df08f8e0ef8cdce1e7
TymurBondar <tymurbondar@outlook.com>
2023-11-29 17:01:35 -0500

    custom animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: { 
-    extend: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-6deg)" },
+          "50%": { transform: "rotate(6deg)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
