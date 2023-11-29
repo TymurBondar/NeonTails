460486d945d7d3245d487fffddfb4ad882fd2f07
TymurBondar <tymurbondar@outlook.com>
2023-11-28 22:49:49 -0500

    basic styling and colors for the home page, added routing for artists and exhibitions

---
+++ frontend/src/routes/Artists.jsx
@@ -0,0 +1,13 @@
+function Artists(){
+    return (
+        <div>
+            <ul>
+                <li>Artist 1</li>
+                <li>Artist 2</li>
+                <li>Artist 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Artists;
--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,19 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 function Root() {
   return (
-    <nav>
-      <ul className="flex justify-between text-2xl">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/artists">Artists</Link>
-        </li>
-        <li>
-          <Link to="/exhibitions">Exhibitions</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="flex flex-col h-screen bg-gradient-to-r from-stone-950 to-zinc-800 text-white">
+      <nav className="p-10 bg-gradient-to-r from-purple-700 to-rose-600 font-bold rounded">
+        <ul className="flex text-2xl space-x-12">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/artists">Artists</Link>
+          </li>
+          <li>
+            <Link to="/exhibitions">Exhibitions</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex flex-1 justify-center items-center font-semibold text-4xl">
+      <Outlet/>
+      </div>
+    </div>
   );
 }
 
