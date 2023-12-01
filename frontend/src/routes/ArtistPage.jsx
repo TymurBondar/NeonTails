7fdd481f07e2605c3b6880a5e49bb417b13852cc
TymurBondar <tymurbondar@outlook.com>
2023-11-30 23:52:19 -0500

    working auth on frontend

--- frontend/src/routes/ArtistPage.jsx
+++ frontend/src/routes/ArtistPage.jsx
@@ -1,34 +1,38 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Card from "../components/Card";
 
-function ArtistPage(){
-    const { id } = useParams();
+function ArtistPage() {
+  const { id } = useParams();
 
-    const [images, setImages] = useState([]);
+  const [images, setImages] = useState([]);
 
-    //add validation so id is a number
-    //fetch database to see if the artists with that id exists
-    //if artists exists, get the artists images in the list
-    useEffect(() => {
-        fetch(`/api/${id}/images`)
-      .then(res => res.json())
-      .then(data => {
+  //add validation so id is a number
+  //fetch database to see if the artists with that id exists
+  //if artists exists, get the artists images in the list
+  useEffect(() => {
+    fetch(`/api/${id}/images`)
+      .then((res) => res.json())
+      .then((data) => {
         let newImages = [];
-        data.forEach(element => {
-            newImages.push(element.image);
+        data.forEach((element) => {
+          newImages.push(element.image);
         });
         setImages(newImages);
-        console.log("log" + newImages);
-        console.log(newImages[0]);
-      })
-    },[]);
+        console.log(newImages);
+      });
+  }, []);
 
-    return (
-        <>
-        This is a page of Artist with the ID of { id }
-
-        </>
-    );
-};
+  return (
+    <div className="flex flex-col items-center text-center mt-12 w-full">
+      This is a page of Artist with the ID of {id}
+      <div className="flex flex-row space-x-24 justify-center w-full mt-24">
+        {images.map((image) => (
+          <Card key={image.id} image={image.link} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default ArtistPage;
